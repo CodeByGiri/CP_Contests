ccf49c884eb24cfcba2d9307b40095caaf01131a
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-03-15 00:41:02 +0530

    C++ Template used have been committed to the repo

---
+++ Template.cpp
@@ -0,0 +1,25 @@
+//This is my C++ template I have followed across all the problems for the contests.
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    // Write solution for ONE test case here
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;      // remove if problem has no test cases
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
